90531ac36ec311bd7b945fd5a1d697c602b72d21
42randy <andystoll@proton.me>
2023-11-07 13:04:47 +0100

    final makefile & .h

--- Makefile
+++ Makefile
@@ -6,22 +6,56 @@
 #    By: astoll <astoll@student.42lausanne.ch>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/10/25 14:29:24 by astoll            #+#    #+#              #
-#    Updated: 2023/11/07 09:47:29 by astoll           ###   ########.fr        #
+#    Updated: 2023/11/07 13:03:37 by astoll           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
 
-SRCS = ft_atoi.c ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
-		ft_isprint.c ft_memcmp.c ft_strncmp.c ft_tolower.c ft_toupper.c \
-		ft_itoa.c ft_strchr.c ft_strdup.c ft_strjoin.c ft_strmapi.c \
-		ft_strnstr.c ft_strrchr.c ft_strtrim.c ft_substr.c ft_bzero.c \
-		ft_calloc.c ft_memchr.c ft_memcpy.c ft_memmove.c ft_memset.c \
-		ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c ft_striteri.c \
-		ft_strlcat.c ft_strlcpy.c ft_strlen.c
+SRCS =	ft_atoi.c		\
+		ft_bzero.c		\
+		ft_calloc.c		\
+		ft_isalnum.c	\
+		ft_isalpha.c	\
+		ft_isascii.c	\
+		ft_isdigit.c	\
+		ft_isprint.c	\
+		ft_itoa.c		\
+		ft_memchr.c		\
+		ft_memcmp.c		\
+		ft_memcpy.c		\
+		ft_memmove.c	\
+		ft_memset.c		\
+		ft_putchar_fd.c	\
+		ft_putendl_fd.c	\
+		ft_putnbr_fd.c	\
+		ft_putstr_fd.c	\
+		ft_split.c		\
+		ft_strchr.c		\
+		ft_strdup.c		\
+		ft_striteri.c	\
+		ft_strjoin.c	\
+		ft_strlcat.c	\
+		ft_strlcpy.c	\
+		ft_strlen.c		\
+		ft_strmapi.c	\
+		ft_strncmp.c	\
+		ft_strnstr.c	\
+		ft_strrchr.c	\
+		ft_strtrim.c	\
+		ft_substr.c		\
+		ft_tolower.c	\
+		ft_toupper.c	\
 
-SRCSB = ft_lstlast.c ft_lstnew.c ft_lstadd_back.c ft_lstadd_front.c ft_lstclear.c \
-		ft_lstdelone.c ft_lstiter.c ft_lstsize.c
+SRCSB =	ft_lstadd_back.c	\
+		ft_lstadd_front.c	\
+		ft_lstclear.c		\
+		ft_lstdelone.c		\
+		ft_lstiter.c		\
+		ft_lstlast.c		\
+		ft_lstmap.c			\
+		ft_lstnew.c			\
+		ft_lstsize.c		\
 
 OBJS = ${SRCS:.c=.o}
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: astoll <astoll@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 11:45:16 by astoll            #+#    #+#             */
-/*   Updated: 2023/11/07 09:47:27 by astoll           ###   ########.fr       */
+/*   Updated: 2023/11/07 13:03:35 by astoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,60 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_atoi(const char *str);
+// Mandatory part
+
+// is
+
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+
+// mem
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-char	*ft_itoa(int n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset(void *b, int c, size_t len);
+
+// str
+
+//char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-void	*ft_memchr(const void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memset(void *b, int c, size_t len);
+
+// to
+
+int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+// put
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
+
+// Bonus part
+
+// structure
 
 typedef struct s_list
 {
@@ -55,13 +76,16 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
-t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstnew(void *content);
+// lst
+
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+//t_list	*ft_lstmap(t_list *lst, void *(*f)(void *));
+t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
 
 #endif
